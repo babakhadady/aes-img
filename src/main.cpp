12ce074f40598f8fe5480039a163e2331c457b3a
Babak <hadadybabak@gmail.com>
2023-08-15 00:30:29 -0700

    fixed mismatched brackets

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "aes.hpp"
 #include "../lib/cs221util/PNG.h"
+#include "aes.hpp"
 
 #include <iostream>
 #include <string>
@@ -35,40 +35,41 @@ int main(int argc, char *argv[])
   for (int i = 0; i < (png->width() * png->height() % 128); i++) {
     i == 0 ? data.push_back({0x80, 0x00, 0x00, 0x00})
            : data.push_back({0x00, 0x00, 0x00, 0x00});
-
-    AES *aes = new AES();
-
-    vector<vector<uint8_t>> data_enc;
-
-    for (int i = 0; i < data.size(); i += 4) {
-      vector<vector<uint8_t>> temp{data[i], data[i + 1], data[i + 2],
-                                   data[i + 3]};
-
-			if (i != 0) {
-				aes->CipherBlockChain
-			}
-
-      aes->AesEncrypt(temp, {0xff, 0xff, 0xff, 0xaf, 0xbf, 0xff, 0xff, 0xff,
-                             0xff, 0xff, 0xff, 0xff, 0xff, 0xcf, 0xdf, 0xff});
-
-      data_enc.insert(data_enc.end(), temp.begin(), temp.end());
-    }
-
-    PNG *out = new PNG(png->width(), png->height());
-
-    int a = 0;
-    for (int i = 0; i < png->width(); i++) {
-      for (int j = 0; j < png->height(); j++) {
-        RGBAPixel *pixel = out->getPixel(i, j);
-        pixel->r = data_enc[a][0];
-        pixel->g = data_enc[a][1];
-        pixel->b = data_enc[a][2];
-        pixel->a = (double)data_enc[a][3] / 255;
-        a++;
-      }
-    }
-
-    file_name = file_name.substr(0, file_name.find(".png")) + "_encrypted.png";
-    cout << "Encryped File Saved to: " << file_name << endl;
-    out->writeToFile(file_name);
   }
+
+  AES *aes = new AES();
+
+  vector<vector<uint8_t>> data_enc;
+
+  for (int i = 0; i < data.size(); i += 4) {
+    vector<vector<uint8_t>> temp{data[i], data[i + 1], data[i + 2],
+                                 data[i + 3]};
+
+    if (i != 0) {
+      /* aes->CipherBlockChain */
+    }
+
+    aes->AesEncrypt(temp, {0xff, 0xff, 0xff, 0xaf, 0xbf, 0xff, 0xff, 0xff, 0xff,
+                           0xff, 0xff, 0xff, 0xff, 0xcf, 0xdf, 0xff});
+
+    data_enc.insert(data_enc.end(), temp.begin(), temp.end());
+  }
+
+  PNG *out = new PNG(png->width(), png->height());
+
+  int a = 0;
+  for (int i = 0; i < png->width(); i++) {
+    for (int j = 0; j < png->height(); j++) {
+      RGBAPixel *pixel = out->getPixel(i, j);
+      pixel->r = data_enc[a][0];
+      pixel->g = data_enc[a][1];
+      pixel->b = data_enc[a][2];
+      pixel->a = (double)data_enc[a][3] / 255;
+      a++;
+    }
+  }
+
+  file_name = file_name.substr(0, file_name.find(".png")) + "_encrypted.png";
+  cout << "Encryped File Saved to: " << file_name << endl;
+  out->writeToFile(file_name);
+}
